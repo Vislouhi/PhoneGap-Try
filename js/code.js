2d4a9d479098b6a3343c9af240868e5d35ecb6c0
Vislouhi <35730180+Vislouhi@users.noreply.github.com>
2018-02-04 23:33:52 +0500

    Add files via upload

--- js/code.js
+++ js/code.js
@@ -1,1 +1,41 @@
-
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+      }
+	  function get4randoms(min,max,tap,ta)
+	  {
+		var randoms = [3];
+		
+		randoms[tap]=ta;
+		
+		if (tap!=0)
+		do{
+		randoms[0]=getRandomInt(min, max);
+		}while(randoms[0]==randoms[tap]);
+		
+		
+		if (tap!=1) do
+		{
+			randoms[1]=getRandomInt(min, max);
+		}while((randoms[0]==randoms[1])||(randoms[1]==randoms[tap]));
+	    if (tap!=2) do
+		{
+			randoms[2]=getRandomInt(min, max);
+		}while((randoms[0]==randoms[2])||(randoms[1]==randoms[2])||(randoms[tap]==randoms[2]));
+		
+		if (tap!=3)do
+		{
+			randoms[3]=getRandomInt(min, max);
+		}while((randoms[0]==randoms[3])||(randoms[1]==randoms[3])||(randoms[2]==randoms[3]));
+		return randoms;
+	  }
+	  
+	  
+	  function genQuestString(val){
+	  
+		
+	  for (var i = 0; i < val; i++) {
+  
+		document.write("| &nbsp &nbsp");
+		}
+	  
+	  }
